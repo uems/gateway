dffeda43957950fd2189ff1f2952cfa6764b34cd
Felipe Mobus <fmobus@gmail.com>
2014-04-23 00:19:07 -0300

    loads arbitrary entities by id
    entity map is still hardcoded

--- spec/lib/client.spec.js
+++ spec/lib/client.spec.js
@@ -1,20 +1,39 @@
 require('../spec_helper');
+
 var expect = require('expect');
+var nock = require('nock');
+
 var clientLib = require('../../lib/client');
 
-
-
-describe("RestClient", function() {
+describe("Generic Rest Client", function() {
   var subject;
+  var nockScope;
   var endpoint = 'http://localhost:9999';
 
   beforeEach(function() {
     subject = clientLib.build(endpoint);
+    nockScope = nock(endpoint);
+  });
+
+  describe("available entities", function() {
+    it("lists them all", function(ƒ) {
+      subject.entities().then(function(entities) {
+        expect(entities).toEqual(['people','groups']);
+      }).µ(ƒ)
+    });
   });
 
   describe("get", function() {
+    var leela = '{ "id": 123, "name": "Turanga Leela" }';
+
+    beforeEach(function() {
+      nockScope.get('/people/123').reply(200, leela);
+    });
+
     it("by entity and id", function(ƒ) {
-      subject.get('person', 123).then(function(result) {
+      subject.get('people', 123).then(function(result) {
+        expect(result.id).toEqual(123);
+        expect(result.name).toEqual('Turanga Leela');
       }).µ(ƒ);
     });
   });

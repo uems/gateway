b0a31650b80c457f75344352bd53195f8aca752c
Felipe Mobus <fmobus@gmail.com>
2014-05-24 13:43:15 -0300

    limpando codigo desnecessario
    esqueleto da rota generica proxy

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,27 +1,14 @@
 var gulp = require('gulp');
 
-var mocha   = require('gulp-mocha'),
-    nodemon = require('gulp-nodemon')
-
-
-var testFiles = [
-  'spec/*.spec.js',
-  'spec/**/*.spec.js'
-];
+var nodemon = require('gulp-nodemon');
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
-gulp.task("test", function() {
-  return gulp.src(testFiles)
-    .pipe(mocha({ reporter: "spec" })
-    .on("error", handleError));
-});
-
 gulp.task('nodemon', function () {
-  return nodemon({ script: 'app.js' })
+  return nodemon({ script: 'app.js' });
 });
 
 gulp.task("default", ["test"]);

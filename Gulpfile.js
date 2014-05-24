4d32902fdd703830310951ebea798ed175ea5cb7
Felipe Mobus <fmobus@gmail.com>
2014-05-24 14:41:35 -0300

    Revert "limpando codigo desnecessario"
    This reverts commit b0a31650b80c457f75344352bd53195f8aca752c.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 
-var nodemon = require('gulp-nodemon');
+var mocha   = require('gulp-mocha'),
+    nodemon = require('gulp-nodemon')
+
+
+var testFiles = [
+  'spec/*.spec.js',
+  'spec/**/*.spec.js'
+];
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
+gulp.task("test", function() {
+  return gulp.src(testFiles)
+    .pipe(mocha({ reporter: "spec" })
+    .on("error", handleError));
+});
+
 gulp.task('nodemon', function () {
-  return nodemon({ script: 'app.js' });
+  return nodemon({ script: 'app.js' })
 });
 
 gulp.task("default", ["test"]);
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 
-var config = require('./lib/config');
-
 require('./lib/middleware')(app);
 require('./lib/routes')(app);
 
-var server = app.listen(process.argv[2] || config.port, function() {
+var server = app.listen(process.argv[2] || 2000, function() {
   console.log('Listening on port %d', server.address().port);
 });
